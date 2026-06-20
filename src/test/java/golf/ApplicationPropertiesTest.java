2b2ac23bcf5fc03e031724b30e09104f9acaeec4
teggr <robin@tegg.me.uk>
2026-06-20 10:06:53 +0100

    Refactor mail configuration to use environment variables and add unit test for property loading

---
+++ src/test/java/golf/ApplicationPropertiesTest.java
@@ -0,0 +1,27 @@
+package golf;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ApplicationPropertiesTest {
+
+    @Test
+    void productionFacingPropertiesAreDrivenByEnvironmentVariables() throws IOException {
+        Properties properties = new Properties();
+
+        try (var input = Files.newInputStream(Path.of("src/main/resources/application.properties"))) {
+            properties.load(input);
+        }
+
+        assertEquals("${MAIL_SMTP_AUTH:false}", properties.getProperty("spring.mail.properties.mail.smtp.auth"));
+        assertEquals("${MAIL_SMTP_STARTTLS_ENABLE:false}", properties.getProperty("spring.mail.properties.mail.smtp.starttls.enable"));
+        assertEquals("${MAIL_SMTP_STARTTLS_REQUIRED:false}", properties.getProperty("spring.mail.properties.mail.smtp.starttls.required"));
+        assertEquals("${APP_BASE_URL:http://localhost:8080}", properties.getProperty("golf.app.base-url"));
+    }
+}
